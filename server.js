5d02e9f97e2fcacf62cd2a3c8142c856186fa2dc
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-07-09 17:33:56 +0530

    Server Updated Text Chat Added

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, { cors: { origin: "*" } }); // Pass the server instance to the socket.io constructor
 let connectedUsers = [];
+let textChatUsers = [];
 
 const port = process.env.PORT || 5000;
 
@@ -14,20 +15,51 @@ io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
   // Add user to connected users set
+
+
+
+  const updateTextChatUsers = (userid, updatedAvailable, updatedRoom) => {
+    textChatUsers = textChatUsers.map((user) => {
+      if (user.userid === userid) {
+        return {
+          ...user,
+          available: updatedAvailable,
+          room: updatedRoom,
+        };
+      }
+      return user;
+    });
+  };
+
+  const updateConnectedUser = (userid, updatedAvailable, updatedRoom) => {
+    connectedUsers = connectedUsers.map((user) => {
+      if (user.userid === userid) {
+        return {
+          ...user,
+          available: updatedAvailable,
+          room: updatedRoom,
+        };
+      }
+      return user;
+    });
+  };
   
-  socket.on("userJoined", ({ userid }) => {
+  socket.on("userJoined", ({ userid,type }) => {
     
     console.log("jee", userid);
+
+   if(userid==null){
+    return;
+   }
+console.log(userid,type);
+   if(type=="video"){
     connectedUsers.push({
       userid: userid,
       available: true,
       room: null,
     });
-    
-    console.log(connectedUsers);
-    io.emit("userCount",{length:connectedUsers.length});
-
-    // Check if there are at least two users connected
+    pair.set(userid,"video")
+    io.emit("videoChatCount",{length:connectedUsers.length});
     if (connectedUsers.length >= 2) {
       const usersArray = Array.from(connectedUsers);
       const excludeme = usersArray.filter(
@@ -41,9 +73,7 @@ io.on("connection", (socket) => {
         const user2 = userid;
         const room = `room-${user1}-${user2}`;
 
-   
-
-
+  
         const updateConnectedUser = (userid, updatedAvailable, updatedRoom) => {
           connectedUsers = connectedUsers.map((user) => {
             if (user.userid === userid) {
@@ -73,124 +103,193 @@ io.on("connection", (socket) => {
       });
       }
 
-      // Select two random users from the connected users set
-      // const randomIndex1 = Math.floor(Math.random() * usersArray.length);
-      // const randomIndex2 = Math.floor(Math.random() * (usersArray.length - 1));
-      // const user1 = usersArray[randomIndex1];
-      // const user2 =
-      //   usersArray[
-      //     randomIndex2 === randomIndex1 ? usersArray.length - 1 : randomIndex2
-      //   ];
-
-      // // Create a room for the two random users
-      // const roomName = `room-${user1}-${user2}`;
-      // socket.join(roomName);
-      // io.to(user1).emit("roomCreated", {
-      //   roomName: roomName,
-      //   remoteUser: user2,
-      // });
-      // io.to(user2).emit("roomCreated", {
-      //   roomName: roomName,
-      //   remoteUser: user1,
-      //   createPermission: true,
-      // });
-      // pair.set(user1, roomName);
-      // pair.set(user2, roomName);
-      // connectedUsers.delete(user1);
-      // connectedUsers.delete(user2);
-      // console.log(connectedUsers);
+      
     }
-  });
-  const updateConnectedUser = (userid, updatedAvailable, updatedRoom) => {
-    connectedUsers = connectedUsers.map((user) => {
-      if (user.userid === userid) {
-        return {
-          ...user,
-          available: updatedAvailable,
-          room: updatedRoom,
-        };
-      }
-      return user;
+   }else{
+    textChatUsers.push({
+      userid: userid,
+      available: true,
+      room: null,
     });
-  };
+    pair.set(userid,"text");
+    io.emit("textChatCount",{length:textChatUsers.length});
 
-  socket.on("newUser",({remoteUserId,currentUserId})=>{
-    console.log("rremoUsre",remoteUserId,"currentUserid",currentUserId);
+    if (textChatUsers.length >= 2) {
+      const usersArray = Array.from(textChatUsers);
+      const excludeme = usersArray.filter(
+        (user) => user.userid !== userid && user.available === true
+      );
 
-    if(remoteUserId==null){
-      const newUser = connectedUsers.filter((user)=> user.userid!==currentUserId && user.available===true);
-      if(newUser.length>0){
-        const randomIndex1 = Math.floor(Math.random() * newUser.length);
-  
-          const user1 = newUser[randomIndex1].userid;
-          const user2 = currentUserId;
-          const room = `room-${user1}-${user2}`;
-  
-          updateConnectedUser(user1,false,room);
-          updateConnectedUser(user2,false,room);
-          
-          socket.join(room);
-          io.to(user1).emit("roomCreated", {
-          roomName: room,
-          remoteUser: user2,
-        });
-        io.to(user2).emit("roomCreated", {
-          roomName: room,
-          remoteUser: user1,
-          createPermission: true,
-        });
+      if (excludeme.length > 0) {
+        const randomIndex1 = Math.floor(Math.random() * excludeme.length);
 
+        const user1 = excludeme[randomIndex1].userid;
+        const user2 = userid;
+        const room = `room-${user1}-${user2}`;
+        updateTextChatUsers(user1,false,room);
+        updateTextChatUsers(user2,false,room);
+        
+        socket.join(room);
+        io.to(user1).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user2,
+      });
+      io.to(user2).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user1,
+        createPermission: true,
+      });
       }
 
-      return;
-  
-    }else{
-      const updateConnectedUser = (userid, updatedAvailable, updatedRoom) => {
-        connectedUsers = connectedUsers.map((user) => {
-          if (user.userid === userid) {
-            return {
-              ...user,
-              available: updatedAvailable,
-              room: updatedRoom,
-            };
-          }
-          return user;
-        });
-      };
       
-      updateConnectedUser(remoteUserId,true,null);
-      
-      socket.to(remoteUserId).emit("userDisconnected");
-  
-      const newUser = connectedUsers.filter((user)=>user.userid!==remoteUserId && user.userid!==currentUserId && user.available===true);
-  
-      if(newUser.length>0){
-        const randomIndex1 = Math.floor(Math.random() * newUser.length);
-  
-          const user1 = newUser[randomIndex1].userid;
-          const user2 = currentUserId;
-          const room = `room-${user1}-${user2}`;
-  
-          updateConnectedUser(user1,false,room);
-          updateConnectedUser(user2,false,room);
-          
-          socket.join(room);
-          io.to(user1).emit("roomCreated", {
-          roomName: room,
-          remoteUser: user2,
-        });
-        io.to(user2).emit("roomCreated", {
-          roomName: room,
-          remoteUser: user1,
-          createPermission: true,
-        });
-  
-      }else{
-        updateConnectedUser(currentUserId,true,null);
-  
-      }
-  
     }
+   }
+    
+    // console.log(connectedUsers);
+   
+
+    // Check if there are at least two users connected
+    
+  });
+ 
+
+  socket.on("newUser",({remoteUserId,currentUserId,type})=>{
+console.log("rremoUsre",remoteUserId,"currentUserid",currentUserId);
+if(currentUserId==null){
+  return;
+}
+if(type=="video"){
+  io.emit("videoChatCount",{length:connectedUsers.length});
+  if(remoteUserId==null){
+    const newUser = connectedUsers.filter((user)=> user.userid!==currentUserId && user.available===true);
+    if(newUser.length>0){
+      const randomIndex1 = Math.floor(Math.random() * newUser.length);
+
+        const user1 = newUser[randomIndex1].userid;
+        const user2 = currentUserId;
+        const room = `room-${user1}-${user2}`;
+
+        updateConnectedUser(user1,false,room);
+        updateConnectedUser(user2,false,room);
+        
+        socket.join(room);
+        io.to(user1).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user2,
+      });
+      io.to(user2).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user1,
+        createPermission: true,
+      });
+      
+
+    }
+
+    return;
+
+  }else{
+   
+    updateConnectedUser(remoteUserId,true,null);
+    
+    socket.to(remoteUserId).emit("userDisconnected");
+
+    const newUser = connectedUsers.filter((user)=>user.userid!==remoteUserId && user.userid!==currentUserId && user.available===true);
+
+    if(newUser.length>0){
+      const randomIndex1 = Math.floor(Math.random() * newUser.length);
+
+        const user1 = newUser[randomIndex1].userid;
+        const user2 = currentUserId;
+        const room = `room-${user1}-${user2}`;
+
+        updateConnectedUser(user1,false,room);
+        updateConnectedUser(user2,false,room);
+        
+        socket.join(room);
+        io.to(user1).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user2,
+      });
+      io.to(user2).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user1,
+        createPermission: true,
+      });
+
+    }else{
+      updateConnectedUser(currentUserId,true,null);
+
+    }
+
+  }
+}else{
+
+  io.emit("videoChatCount",{length:textChatUsers.length});
+  if(remoteUserId==null){
+    const newUser = textChatUsers.filter((user)=> user.userid!==currentUserId && user.userid!==undefined && user.available===true);
+    if(newUser.length>0){
+      const randomIndex1 = Math.floor(Math.random() * newUser.length);
+
+        const user1 = newUser[randomIndex1].userid;
+        const user2 = currentUserId;
+        const room = `room-${user1}-${user2}`;
+
+        updateTextChatUsers(user1,false,room);
+        updateTextChatUsers(user2,false,room);
+        
+        socket.join(room);
+        io.to(user1).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user2,
+      });
+      io.to(user2).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user1,
+        createPermission: true,
+      });
+
+    }
+
+    return;
+
+  }else{
+    
+    
+    updateTextChatUsers(remoteUserId,true,null);
+    
+    socket.to(remoteUserId).emit("userDisconnected");
+
+    const newUser = textChatUsers.filter((user)=>user.userid!==remoteUserId && user.userid!==undefined && user.userid!==currentUserId && user.available===true);
+
+    if(newUser.length>0){
+      const randomIndex1 = Math.floor(Math.random() * newUser.length);
+
+        const user1 = newUser[randomIndex1].userid;
+        const user2 = currentUserId;
+        const room = `room-${user1}-${user2}`;
+
+        updateTextChatUsers(user1,false,room);
+        updateTextChatUsers(user2,false,room);
+        
+        socket.join(room);
+        io.to(user1).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user2,
+      });
+      io.to(user2).emit("roomCreated", {
+        roomName: room,
+        remoteUser: user1,
+        createPermission: true,
+      });
+
+    }else{
+      updateTextChatUsers(currentUserId,true,null);
+
+    }
+
+  }
+}
     
 
 
@@ -228,45 +327,73 @@ io.on("connection", (socket) => {
     io.to(remoteUser).emit("messageReceived", message);
   });
   // Handle user disconnection
-  socket.on("dis", ({ currentUserId, remoteUser }) => {
-    // console.log("User disconnected:", data);
-    // Remove user from connected users set
-    connectedUsers.delete(currentUserId);
-    // console.log(connectedUsers);
-    console.log(currentUserId, remoteUser);
-    io.to(remoteUser).emit("userDisconnected", currentUserId);
-  });
+  // socket.on("dis", ({ currentUserId, remoteUser }) => {
+  //   // console.log("User disconnected:", data);
+  //   // Remove user from connected users set
+  //   connectedUsers.delete(currentUserId);
+  //   // console.log(connectedUsers);
+  //   console.log(currentUserId, remoteUser);
+  //   io.to(remoteUser).emit("userDisconnected", currentUserId);
+  // });
 
   socket.on("disconnect", () => {
-    io.emit("userCount",{length:connectedUsers.length});
-    const leavinguser= connectedUsers.find((user)=>user.userid ==socket.id);
-    connectedUsers= connectedUsers.filter((user)=>user.userid!==socket.id);
-    console.log(socket.id, "disconnected",leavinguser);
-    if(leavinguser!=null && leavinguser.room!=null){
-      const findRemoteUser = connectedUsers.find((user)=>user.room==leavinguser.room);
-      const updateConnectedUser = (userid, updatedAvailable, updatedRoom) => {
-        connectedUsers = connectedUsers.map((user) => {
-          if (user.userid === userid) {
-            return {
-              ...user,
-              available: updatedAvailable,
-              room: updatedRoom,
-            };
-          }
-          return user;
-        });
-      };
-
-      updateConnectedUser(findRemoteUser.userid,true,null);
-      
-      socket.to(findRemoteUser.userid).emit("userDisconnected");
-      socket.disconnect();
+    const roomType = pair.get(socket.id);
+    if(roomType==undefined){
+      return;
     }
-    // console.log(findUser);
-    console.log(leavinguser,connectedUsers);
-    // console.log(findUser[0]);
-    // delete connectedUsers[findUser[0]];
-  });
+
+    console.log("Disconnected",socket.id,roomType,connectedUsers);
+
+
+if(roomType=="video"){
+  
+  const leavinguser= connectedUsers.find((user)=>user.userid ==socket.id);
+  if(!leavinguser){
+    return;
+  }
+  if(!leavinguser.room){
+    connectedUsers= connectedUsers.filter((user)=>user.userid!==undefined && user.userid!==leavinguser.userid);
+    return;
+  }
+  connectedUsers= connectedUsers.filter((user)=>user.userid!==socket.id);
+  console.log(socket.id, "disconnected",leavinguser);
+  
+    const findRemoteUser = connectedUsers.find((user)=>user.room==leavinguser.room);
+
+    updateConnectedUser(findRemoteUser.userid,true,null);
+    
+    socket.to(findRemoteUser.userid).emit("userDisconnected");
+    io.emit("videoChatCount",{length:connectedUsers.length});
+    socket.disconnect();
+  
+  // console.log(findUser);
+  console.log(leavinguser,connectedUsers,"here");
+  // console.log(findUser[0]);
+  // delete connectedUsers[findUser[0]];
+}else{
+
+  const leavinguser= textChatUsers.find((user)=>user.userid ==socket.id);
+  if(!leavinguser){
+    return;
+  }
+
+  if(!leavinguser.room){
+    textChatUsers= textChatUsers.filter((user)=>user.userid!==undefined && user.userid!==leavinguser.userid);
+    return;
+  }
+  textChatUsers= textChatUsers.filter((user)=>user.userid!==socket.id);
+  console.log(socket.id, "disconnected",leavinguser);
+    const findRemoteUser = textChatUsers.find((user)=>user.room==leavinguser.room);
+    updateTextChatUsers(findRemoteUser.userid,true,null);
+    console.log(findRemoteUser);
+    socket.to(findRemoteUser.userid).emit("userDisconnected");
+    io.emit("textChatCount",{length:textChatUsers.length});
+    socket.disconnect();
+  }
+  // console.log(findUser);
+  // console.log(leavinguser,connectedUsers,textChatUsers);
+
+});
 });
 
 app.get("/", (req, res) => {
--- test.js
+++ test.js
@@ -4,12 +4,15 @@ const userid2 = 2;
 
 ap.set(userid1,userid2);
 ap.set(2,3);
-ap.set(4,3);
+ap.set(2,4);
 ap.set(6,7);
 
 
 const fun=()=>{
- console.log(ap.values())
+const a = ap.get(0);
+if(a==undefined){
+    console.log("yes")
+}
 }
 
 fun();
